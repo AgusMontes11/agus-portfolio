60dfe3e203c04c0b1ad51bcafb83846e41606c84
AgusMontess11 <gabriel.montes@alumnos.frm.utn.edu.ar>
2026-01-12 23:35:36 -0300

    Iniciando repo

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <section>
+      <h2>Hola, soy Agus</h2>
+      <p>
+        Desarrollador de software enfocado en crear aplicaciones web modernas.
+      </p>
+    </section>
+  );
+}
